1460b81389f6a7354a0eb356df63a0ee776167e0
Sam Bonnekamp <sam@bonnekamp.net>
2025-10-11 17:37:19 +0800

    added helper functions kinda

--- main.c
+++ main.c
@@ -14,6 +14,26 @@ typedef struct{
   struct sockaddr_in peer_addr;
 }connection_info;
 
+typedef struct{
+  uint8_t method;
+  char* host;
+}http_connection;
+
+typedef struct{
+  uint16_t code;
+  char* content_type;
+}http_response;
+
+void format_http_response(http_response *res, char* str, size_t limit){
+  char* res_str = "\
+HTTP/2 404\r\n\
+Content-Type: text/html\r\n\
+Content-Length: 21\r\n\
+Connection: Closed\r\n\r\n\
+<h1>Hello world</h1>\r\n";
+
+  strncpy(str, res_str, limit);
+}
 
 int open_connection(uint16_t PORT, connection_info *ret){
   ret->sockfd = socket(AF_INET, SOCK_STREAM, 0); //open a socket
@@ -42,16 +62,27 @@ int open_connection(uint16_t PORT, connection_info *ret){
 int main(int argc, char* argv[]){
   connection_info ci;
   char in_buf[1024];
+
   if(open_connection(8080, &ci) != 0){
     return 1;
   }
 
   socklen_t size_of_peer = sizeof(ci.peer_addr);
   int peer_socket = accept(ci.sockfd, (struct sockaddr *)&ci.my_addr, &size_of_peer);
+
   if(peer_socket == -1){
     perror("accept error");
     return 1;
   }
+
+  http_response res = {0};
+  res.code = 200;
+  res.content_type = "text/html";
+
   ssize_t in_val = read(peer_socket, in_buf, 1023);
+  printf("%s\n", in_buf);
+  format_http_response(&res, in_buf, 1024);
+  printf("%s\n", in_buf);
   send(peer_socket, in_buf, strlen(in_buf), 0);
+
 }
