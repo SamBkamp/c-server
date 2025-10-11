4bec9db64a2d005e2108d70665d703cb7fb8a8d2
Sam Bonnekamp <sam@bonnekamp.net>
2025-10-11 16:53:06 +0800

    moved connection stuff to function

--- main.c
+++ main.c
@@ -5,39 +5,53 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <string.h>
+#include <unistd.h>
+#include <stdint.h>
 
-#define PORT 8080
+typedef struct{
+  int sockfd;
+  struct sockaddr_in my_addr;
+  struct sockaddr_in peer_addr;
+}connection_info;
 
-int main(int argc, char* argv[]){
-  struct sockaddr_in my_addr, peer_addr; //define the two socket addresses7
 
-  int s = socket(AF_INET, SOCK_STREAM, 0); //open a socket
-  if(s == -1){
+int open_connection(uint16_t PORT, connection_info *ret){
+  ret->sockfd = socket(AF_INET, SOCK_STREAM, 0); //open a socket
+  if(ret->sockfd == -1){
     perror("socket error");
-    return 1;
+    return -1;
   }
 
-  memset(&my_addr, 0, sizeof(my_addr)); //reset our socket address structs
-  my_addr.sin_family = AF_INET; //were using IP
-  my_addr.sin_port = htons(PORT); //turn from host byte order (little endian) to network byte (big endian)
-  my_addr.sin_addr = (struct in_addr){INADDR_ANY}; //accept connections from anywhere (0.0.0.0)
+  memset(&ret->my_addr, 0, sizeof(ret->my_addr)); //reset our socket address structs
+  ret->my_addr.sin_family = AF_INET; //were using IP
+  ret->my_addr.sin_port = htons(PORT); //turn from host byte order (little endian) to network byte (big endian)
+  ret->my_addr.sin_addr = (struct in_addr){INADDR_ANY}; //accept connections from anywhere (0.0.0.0)
   //only accepts the in_addr struct which takes one value, for some reason.
 
-
-  if(bind(s, (struct sockaddr *)&my_addr, sizeof(my_addr)) != 0){ //bind our socket to our address
+  if(bind(ret->sockfd, (struct sockaddr *)&ret->my_addr, sizeof(ret->my_addr)) != 0){ //bind our socket to our address
     perror("bind error");
-    return 1;
+    return -1;
   }
-  if(listen(s, 5) == -1){ //set our socket as listening
+  if(listen(ret->sockfd, 5) == -1){ //set our socket as listening
     perror("listen error");
+    return -1;
+  }
+  return 0;
+}
+
+int main(int argc, char* argv[]){
+  connection_info ci;
+  char in_buf[1024];
+  if(open_connection(8080, &ci) != 0){
     return 1;
   }
 
-  socklen_t size_of_peer = sizeof(peer_addr);
-  int a = accept(s, (struct sockaddr *)&my_addr, &size_of_peer);
-
-  if(a == -1){
+  socklen_t size_of_peer = sizeof(ci.peer_addr);
+  int peer_socket = accept(ci.sockfd, (struct sockaddr *)&ci.my_addr, &size_of_peer);
+  if(peer_socket == -1){
     perror("accept error");
     return 1;
   }
+  ssize_t in_val = read(peer_socket, in_buf, 1023);
+  send(peer_socket, in_buf, strlen(in_buf), 0);
 }
