2b14d15f5197d6ae65f1254802548e7043cbdf6e
Sam Bonnekamp <sam@bonnekamp.net>
2025-10-15 00:17:47 +0800

    minimising main file

---
+++ Makefile
@@ -0,0 +1,3 @@
+
+server:conn.c main.c
+	gcc -Wall -Wextra conn.c main.c -o server
--- main.c
+++ main.c
@@ -12,27 +12,20 @@
 #include "prot.h"
 #include "conn.h"
 
+#define PORT 8080
 #define SECONDS_PER_HOUR 3600
 #define HK_OFFSET 28800 //8 hours
 
-void format_http_response(http_response *res, char* str, size_t limit){
-  char* res_str = "\
-HTTP/2 404\r\n\
-Content-Type: text/html\r\n\
-Content-Length: 21\r\n\
-Connection: Closed\r\n\r\n\
-<h1>Hello world</h1>\r\n";
 
-  strncpy(str, res_str, limit);
-}
-
-int main(int argc, char* argv[]){
+int main(){
   connection_info ci;
   char in_buf[1024];
 
-  if(open_connection(8080, &ci) != 0){
+  if(open_connection(PORT, &ci) != 0){
     return 1;
   }
+
+  printf("Started server on port %d\n", PORT);
 
   socklen_t size_of_peer = sizeof(ci.peer_addr);
   while(1){
@@ -42,19 +35,16 @@ int main(int argc, char* argv[]){
       perror("accept error");
       return 1;
     }
+    read(peer_socket, in_buf, 1023);
 
-    http_response res = {0};
-    res.code = 200;
-    res.content_type = "text/html";
-
-    memset(in_buf, 0, 1023);
-    ssize_t in_val = read(peer_socket, in_buf, 1023);
-    printf("%s\n", in_buf);
-    memset(in_buf, 0, 1023);
-    unsigned long time_now = time(NULL);
-    sprintf(in_buf, "%lu", time_now+HK_OFFSET);
-    printf("%s\n", in_buf);
+    if(strncmp(in_buf, "nasdaq", 6) == 0){
+      sprintf(in_buf, "%u", 22563);
+    }else{
+      unsigned long time_now = time(NULL);
+      sprintf(in_buf, "%lu", time_now+HK_OFFSET);
+    }
     send(peer_socket, in_buf, strlen(in_buf), 0);
+    close(peer_socket);
   }
 
 
