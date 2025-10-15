066ffaacf66c1673c8c3787da2ed618ba599d1f7
Sam Bonnekamp <sam@bonnekamp.net>
2025-10-15 22:06:38 +0800

    cleaned up json parsing a whole bunch

--- parse.c
+++ parse.c
@@ -5,25 +5,28 @@
 #include "prot.h"
 #include "parse.h"
 
+typedef struct{
+  char key[20];
+  char value[20];
+}kv_pair;
+
+void parse_kv(char* test){
+  kv_pair pair = {0};
+  char* token = strtok(test, ":");
+  if(token != NULL)
+    strncpy(pair.key, token, 20);
+  token = strtok(NULL, ":");
+  if(token != NULL)
+    strncpy(pair.value, token, 20);
+  printf("key: %s\nvalue: %s\n", pair.key, pair.value);
+}
+
 void json_parse(){
   const char *str = "{\"symbol\":\"TEM\",\"name\":\"Tempus AI, Inc.\",\"exchange\":\"NASDAQ\",\"mic_code\":\"XNGS\",\"currency\":\"USD\",\"datetime\":\"2025-10-14\",\"timestamp\":1760448600,\"last_quote_at\":1760448600,\"open\":\"92.11000\",\"high\":\"93.90600\",\"low\":\"87.30000\",\"close\":\"89.87000\",\"volume\":\"6952000\",\"previous_close\":\"94.47000\",\"change\":\"-4.60000\",\"percent_change\":\"-4.86927\",\"average_volume\":\"8553950\",\"is_market_open\":false,\"fifty_two_week\":{\"low\":\"31.36000\",\"high\":\"104.32000\",\"low_change\":\"58.51000\",\"high_change\":\"-14.45000\",\"low_change_percent\":\"186.57526\",\"high_change_percent\":\"-13.85161\",\"range\":\"31.360001 - 104.320000\"}}";
-  char buff[30];
-  size_t offset = 1;
-  size_t in_string = 0; //keep track of if were in a string
-  for(int i = 0; i < 3; i++){
-    int j = 0; //buff index
-    memset(buff, 0, 30); //reset buffer
-    //only quit when a comma is reached AND we're not in a string
-    while(str[j+offset] != ',' || in_string > 0){
-      buff[j] = str[j+offset];
-      if(str[j+offset] == '"')
-	in_string = (in_string+1)%2; //toggle in_string when " is reached
-      j++;
-    }
-    buff[j] = 0;
-    offset += j+1; //+1 to jump over delim
-    printf("%s\n", buff);
-  }
+
+
+  char test[] = "\"symbol\":\"TEM\"";
+  parse_kv(test);
 }
 
 
